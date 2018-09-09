2a6f254f6a9641c63488387a7867c4bd4394652f
sunny03 <x03177@gmail.com>
2018-09-09 22:24:27 +0800

    week1

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,3 +1,8 @@
 function printStars(n) {
-  
+    for(var i=1; i<=n ;i++ ){
+        console.log('*');
+    }
 }
+printStars(1)
+printStars(3)
+printStars(6)
--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,10 @@
 function reverse(str) {
-
+    var word = ''
+    for(var i=str.length-1; i>=0; i--){
+    word += str[i]
+    }
+    return word
 }
+
+console.log(reverse('cat'))
+console.log(reverse('1,2,3,4,5,6,7,8,9'))
